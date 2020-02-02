aacb80838a90e21f6c9d2bcb203be57eed3b0876
Michael Tsafrir <michaeltsafrir@gmail.com>
2020-02-02 07:30:01 +0200

    added simple design layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,21 +4,26 @@ import { HomePage } from '../pages/homePage';
 import { LoginPage } from '../pages/loginPage';
 import { RegisterPage } from '../pages/registerPage';
 import { LibraryPage } from '../pages/libraryPage';
-import logo from '../../images/logo.svg';
+import logo from '../../images/logo.png';
 import './app.css';
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Switch>
-					<Route path='/' exact component={HomePage} />
-					<Route path='/login' exact component={LoginPage} />
-					<Route path='/register' exact component={RegisterPage} />
-					<Route path='/library/:videoID' exact component={LibraryPage} />
-					<Route path='/' render={() => <div>404</div>} />
-				</Switch>
-			</BrowserRouter>
+			<img src={logo} className="App-logo" alt="logo" />
+			<div className="Container">
+				<div className="Main">
+					<BrowserRouter>
+						<Switch>
+							<Route path='/' exact component={HomePage} />
+							<Route path='/login' exact component={LoginPage} />
+							<Route path='/register' exact component={RegisterPage} />
+							<Route path='/library/:videoID' exact component={LibraryPage} />
+							<Route path='/' render={() => <div>404</div>} />
+						</Switch>
+					</BrowserRouter>
+				</div>
+			</div>
 		</div>
 	);
 };
