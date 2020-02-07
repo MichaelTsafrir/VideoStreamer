d78c0f02b497f3cc3e98f946184396a88e9dca07
Michael Tsafrir <michaeltsafrir@gmail.com>
2020-02-07 06:58:15 +0200

    Video list visibility

--- src/components/pages/library/Video/Video.tsx
+++ src/components/pages/library/Video/Video.tsx
@@ -24,13 +24,16 @@ const Video: React.FC<Props> = (props) => {
 				<tbody>
 					<tr>
 						<td rowSpan={3}><img src={videoPlaceholder} alt={video.name} /></td>
-						<td><label>{video.name}</label></td>
+						<td className="info-col">Name:</td>
+						<td className="input-col"><label>{video.name}</label></td>
 					</tr>
 					<tr>
-						<td><label>{video.description}</label></td>
+						<td className="info-col">Description:</td>
+						<td className="input-col"><label>{video.description}</label></td>
 					</tr>
 					<tr>
-						<td><label>{dayjs(video.addDate).format('DD/MM/YY HH:mm')}</label></td>
+						<td className="info-col">Added:</td>
+						<td className="input-col"><label>{dayjs(video.addDate).format('DD/MM/YY HH:mm')}</label></td>
 					</tr>
 				</tbody>
 			</table>
